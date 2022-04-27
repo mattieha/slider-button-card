8bd2152ce17c8901f549ade80fd7e9282a25dd44
Liz Sugar <liz@lizsugar.me>
2022-04-27 16:37:32 -0700

    cleanup

--- src/controllers/input-number-controller.ts
+++ src/controllers/input-number-controller.ts
@@ -5,15 +5,12 @@ import { SliderConfig } from '../types';
 export class InputNumberController extends Controller {
   _targetValue;
   _invert = false;
-  // _min;
-  // _max;
 
   get _value(): number {
     return this.stateObj.state;
   }
 
   set _value(value) {
-    //value = percentageToValue(value, this._min, this._max);
     this._hass.callService('input_number', 'set_value', {
       // eslint-disable-next-line @typescript-eslint/camelcase
       entity_id: this.stateObj.entity_id,
@@ -28,22 +25,6 @@ export class InputNumberController extends Controller {
   get _max(): number {
     return this.stateObj.attributes.max;
   }
-
-  // get _targetValue(): number {
-  //   return this._value;
-  // }
-
-  // set _targetValue(value: number) {
-  //   if (value !== this.targetValue) {
-  //     if (value > this._min) {
-  //       value = this._min;
-  //     }
-  //     if (value > this._max) {
-  //       value = this._max;
-  //     }
-  //     this._targetValue = value;
-  //   }
-  // }
 
   get isValuePercentage(): boolean {
     return false;
