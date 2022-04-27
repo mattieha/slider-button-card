a1050f090662ef41f11dce033de86c0947e6d4c4
Liz Sugar <liz@lizsugar.me>
2022-04-27 16:37:32 -0700

    cleanup

--- dist/slider-button-card.js
+++ dist/slider-button-card.js
@@ -5735,13 +5735,10 @@ class InputNumberController extends Controller {
         super(...arguments);
         this._invert = false;
     }
-    // _min;
-    // _max;
     get _value() {
         return this.stateObj.state;
     }
     set _value(value) {
-        //value = percentageToValue(value, this._min, this._max);
         this._hass.callService('input_number', 'set_value', {
             // eslint-disable-next-line @typescript-eslint/camelcase
             entity_id: this.stateObj.entity_id,
@@ -5754,20 +5751,6 @@ class InputNumberController extends Controller {
     get _max() {
         return this.stateObj.attributes.max;
     }
-    // get _targetValue(): number {
-    //   return this._value;
-    // }
-    // set _targetValue(value: number) {
-    //   if (value !== this.targetValue) {
-    //     if (value > this._min) {
-    //       value = this._min;
-    //     }
-    //     if (value > this._max) {
-    //       value = this._max;
-    //     }
-    //     this._targetValue = value;
-    //   }
-    // }
     get isValuePercentage() {
         return false;
     }
