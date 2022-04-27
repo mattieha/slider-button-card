3a6f8851ba87b1846a1c3d1165a05f127de59b7f
Liz Sugar <liz@lizsugar.me>
2022-04-26 17:31:25 -0700

    Add input_number domain to slider-button-card
    Outstanding bug: Rounding error when input_number step is set to certain values (observed easily with step set to 2 or 3)

--- dist/slider-button-card.js
+++ dist/slider-button-card.js
@@ -5735,11 +5735,13 @@ class InputNumberController extends Controller {
         super(...arguments);
         this._invert = false;
     }
+    // _min;
+    // _max;
     get _value() {
         return this.stateObj.state;
     }
     set _value(value) {
-        value = value / (this._max - this._min);
+        //value = percentageToValue(value, this._min, this._max);
         this._hass.callService('input_number', 'set_value', {
             // eslint-disable-next-line @typescript-eslint/camelcase
             entity_id: this.stateObj.entity_id,
@@ -5752,11 +5754,28 @@ class InputNumberController extends Controller {
     get _max() {
         return this.stateObj.attributes.max;
     }
+    // get _targetValue(): number {
+    //   return this._value;
+    // }
+    // set _targetValue(value: number) {
+    //   if (value !== this.targetValue) {
+    //     if (value > this._min) {
+    //       value = this._min;
+    //     }
+    //     if (value > this._max) {
+    //       value = this._max;
+    //     }
+    //     this._targetValue = value;
+    //   }
+    // }
+    get isValuePercentage() {
+        return false;
+    }
     get _step() {
         return this.stateObj.attributes.step;
     }
     get label() {
-        return `${this.value} ${this.stateObj.attributes.unit_of_measurement}`;
+        return this.stateObj.attributes.unit_of_measurement ? `${this.targetValue} ${this.stateObj.attributes.unit_of_measurement}` : `${this.targetValue}`;
     }
 }
 
--- src/controllers/input-number-controller.ts
+++ src/controllers/input-number-controller.ts
@@ -1,15 +1,19 @@
 import { Controller } from './controller';
+import { normalize, percentageToValue, toPercentage } from '../utils';
+import { SliderConfig } from '../types';
 
 export class InputNumberController extends Controller {
   _targetValue;
   _invert = false;
+  // _min;
+  // _max;
 
   get _value(): number {
     return this.stateObj.state;
   }
 
   set _value(value) {
-    value = value / (this._max - this._min);
+    //value = percentageToValue(value, this._min, this._max);
     this._hass.callService('input_number', 'set_value', {
       // eslint-disable-next-line @typescript-eslint/camelcase
       entity_id: this.stateObj.entity_id,
@@ -25,12 +29,32 @@ export class InputNumberController extends Controller {
     return this.stateObj.attributes.max;
   }
 
+  // get _targetValue(): number {
+  //   return this._value;
+  // }
+
+  // set _targetValue(value: number) {
+  //   if (value !== this.targetValue) {
+  //     if (value > this._min) {
+  //       value = this._min;
+  //     }
+  //     if (value > this._max) {
+  //       value = this._max;
+  //     }
+  //     this._targetValue = value;
+  //   }
+  // }
+
+  get isValuePercentage(): boolean {
+    return false;
+  }
+
   get _step(): number {
     return this.stateObj.attributes.step;
   }
 
   get label(): string {
-    return `${this.value} ${this.stateObj.attributes.unit_of_measurement}`;
+    return this.stateObj.attributes.unit_of_measurement ? `${this.targetValue} ${this.stateObj.attributes.unit_of_measurement}` : `${this.targetValue}`;
   }
 
 }
